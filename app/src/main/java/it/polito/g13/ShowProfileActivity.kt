dbfc4de523404d423610ae4c011a8c034760cfbb
Luciuslucius1 <giorgiopoma37@gmail.com>
2023-03-31 19:20:18 +0200

    try shared

--- app/src/main/java/it/polito/g13/ShowProfileActivity.kt
+++ app/src/main/java/it/polito/g13/ShowProfileActivity.kt
@@ -1,9 +1,12 @@
 package it.polito.g13
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.SharedPreferences
 
 class ShowProfileActivity : AppCompatActivity() {
+    val sharedPreference:SharedPreferences =  getSharedPreferences("preferences", MODE_PRIVATE)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_profile)
