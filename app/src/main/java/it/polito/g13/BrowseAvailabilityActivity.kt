421595da384ef3b54ac17cea446f71f7d80faef3
AleDeGregorio <alessio.degregorio.98@gmail.com>
2023-04-22 15:56:00 +0200

    disable date selection before today

--- app/src/main/java/it/polito/g13/BrowseAvailabilityActivity.kt
+++ app/src/main/java/it/polito/g13/BrowseAvailabilityActivity.kt
@@ -1,10 +1,14 @@
 package it.polito.g13
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
@@ -12,7 +16,11 @@ import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 import com.stacktips.view.CalendarListener
 import com.stacktips.view.CustomCalendarView
+import com.stacktips.view.DayDecorator
+import com.stacktips.view.DayView
+import com.stacktips.view.utils.CalendarUtils
 import it.polito.g13.ui.main.ReservationFragment
+import java.text.SimpleDateFormat
 import java.util.*
 
 
@@ -67,22 +75,43 @@ class BrowseAvailabilityActivity : AppCompatActivity(), NavigationView.OnNavigat
         }
 
         calendarView.setCalendarListener(object : CalendarListener {
+            @SuppressLint("SimpleDateFormat")
+            @RequiresApi(Build.VERSION_CODES.O)
             override fun onDateSelected(date: Date) {
+                val today = Calendar.getInstance().time
+
+                val sdf = SimpleDateFormat("yyyy-MM-dd")
+                val formattedDate = sdf.parse(sdf.format(date))
+                val formattedToday = sdf.parse(sdf.format(today))
+
                 //select LinearLayout to be inflated
                 val bookReservationContainer = findViewById<LinearLayout>(R.id.bookReservationContainer)
-                //get available reservations for this specific date
-                val availableReservations = layoutInflater.inflate(R.layout.availability_box, bookReservationContainer, false)
 
-                //clean the view removing previously showed reservations for another date
-                bookReservationContainer.removeAllViews()
-                //show available reservations for this date
-                bookReservationContainer.addView(availableReservations)
+                if(formattedDate >= formattedToday) {
+                    //get available reservations for this specific date
+                    val availableReservations = layoutInflater.inflate(R.layout.availability_box, bookReservationContainer, false)
+
+                    //clean the view removing previously showed reservations for another date
+                    bookReservationContainer.removeAllViews()
+                    //show available reservations for this date
+                    bookReservationContainer.addView(availableReservations)
+                }
+                else {
+                    //clean the view removing previously showed reservations for another date
+                    bookReservationContainer.removeAllViews()
+                }
             }
 
             override fun onMonthChanged(date: Date) {
                 //do nothing
             }
         })
+
+        //adding calendar day decorators
+        val decorators: MutableList<DayDecorator> = ArrayList()
+        decorators.add(DisabledColorDecorator())
+        calendarView.decorators = decorators
+        calendarView.refreshCalendar(currentCalendar)
     }
 
     //handle toolbar items
@@ -99,5 +128,13 @@ class BrowseAvailabilityActivity : AppCompatActivity(), NavigationView.OnNavigat
         }
         drawerLayout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    private class DisabledColorDecorator : DayDecorator {
+        override fun decorate(dayView: DayView) {
+            if (CalendarUtils.isPastDay(dayView.date)) {
+                dayView.setTextColor(Color.GRAY)
+            }
+        }
     }
 }
